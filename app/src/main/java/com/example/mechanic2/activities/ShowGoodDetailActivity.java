eeb16c067ad9658d783a3e67c12893767e07d4db
abcdandroid <abcdandroid@github.com>
2020-05-31 17:51:07 +0430

    download music completed start to design pin view

--- app/src/main/java/com/example/mechanic2/activities/ShowGoodDetailActivity.java
+++ app/src/main/java/com/example/mechanic2/activities/ShowGoodDetailActivity.java
@@ -2,45 +2,81 @@ package com.example.mechanic2.activities;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieDrawable;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
+import com.downloader.Error;
+import com.downloader.OnDownloadListener;
+import com.downloader.OnPauseListener;
+import com.downloader.OnProgressListener;
+import com.downloader.OnStartOrResumeListener;
+import com.downloader.PRDownloader;
+import com.downloader.Progress;
+import com.downloader.Status;
+import com.downloader.request.DownloadRequest;
 import com.example.mechanic2.R;
+import com.example.mechanic2.adapters.StoreRecyclerAdapter;
 import com.example.mechanic2.adapters.ViewPagerAdapter;
+import com.example.mechanic2.app.Application;
+import com.example.mechanic2.app.SharedPrefUtils;
 import com.example.mechanic2.app.app;
 import com.example.mechanic2.fragments.AdsViewPagerFragment;
 import com.example.mechanic2.fragments.ShowThumbnailFragment;
 import com.example.mechanic2.interfaces.ThumbnailViewPagerState;
 import com.example.mechanic2.models.Ads;
 import com.example.mechanic2.models.Car;
+import com.example.mechanic2.models.Good;
 import com.example.mechanic2.models.Goood;
 import com.example.mechanic2.views.MyTextView;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.gson.Gson;
+import com.hmomeni.progresscircula.ProgressCircula;
 import com.merhold.extensiblepageindicator.ExtensiblePageIndicator;
+import com.mikhaellopez.circularimageview.CircularImageView;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class ShowGoodDetailActivity extends AppCompatActivity {
+public class ShowGoodDetailActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
     private ViewPager viewpager;
     private ViewPagerAdapter adapter;
     int ci = 0;
-    private ExtensiblePageIndicator flexibleIndicator;
+    private Activity activity = this;
+    private Context context = activity;
+
+
     private MyTextView goodName;
     private ImageView carIcon;
     private MyTextView suitableCars;
@@ -50,34 +86,123 @@ public class ShowGoodDetailActivity extends AppCompatActivity {
     private ImageView warrantyIcon;
     private MyTextView warrantyName;
     private ImageView stateIcon;
-    private Activity activity = this;
+    private MyTextView stateText;
+    private LinearLayout parent;
+    private MyTextView desc;
 
+
+    private MediaPlayer mediaPlayer;
+    /**/
+    private SeekBarUpdater seekBarUpdater;
+    private ImageView startDownload;
+    private ImageView ivPlayPause;
+    private ProgressCircula progressCirculaSound;
+    private LottieAnimationView ltPlayPause;
+    private TextView percentDone;/**/
+    private SeekBar sbProgress;
+    Goood goood;
+    private ExtensiblePageIndicator extensiblePageIndicator;
+    String audioAddress;
+    private TextView price;
+
+    private Handler mSeekbarUpdateHandler = new Handler();
+    private Runnable mUpdateSeekbar = new Runnable() {
+        @Override
+        public void run() {
+            if (mediaPlayer != null) {
+                sbProgress.setProgress(mediaPlayer.getCurrentPosition());
+                mSeekbarUpdateHandler.postDelayed(this, 50);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_good_detail);
-        viewpager = findViewById(R.id.viewpager);
-        flexibleIndicator = findViewById(R.id.flexibleIndicator);
-        goodName = findViewById(R.id.good_name);
-        carIcon = findViewById(R.id.car_icon);
-        suitableCars = findViewById(R.id.suitable_cars);
-        factoryIcon = findViewById(R.id.factory_icon);
-        companyName = findViewById(R.id.company_name);
-        countryName = findViewById(R.id.country_name);
-        warrantyIcon = findViewById(R.id.warranty_icon);
-        warrantyName = findViewById(R.id.warranty_name);
-        stateIcon = findViewById(R.id.state_icon);
+        initViews();
+        sbProgress.setEnabled(false);
+        //seekBarUpdater = new SeekBarUpdater();
         adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        ExtensiblePageIndicator extensiblePageIndicator = findViewById(R.id.flexibleIndicator);
-        Goood goood = (Goood) getIntent().getSerializableExtra("good");
-
+        goood = (Goood) getIntent().getSerializableExtra("good");
         if (goood == null) {
             return;
         }
         binder(goood);
+        initViewPager();
+
+        audioAddress = context.getExternalFilesDir("voice/mp3").getAbsolutePath() + goood.getVoice().substring(goood.getVoice().lastIndexOf("/"));
+
+        String url = goood.getVoice();
+        File file = new File(context.getExternalFilesDir("voice/mp3").getAbsolutePath() + url.substring(url.lastIndexOf("/")));
+        if (file.exists() && file.length() == goood.getFileSize()) {
+            mediaPlayer = MediaPlayer.create(Application.getContext(), Uri.parse(audioAddress));
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    seekBarUpdater = new SeekBarUpdater();
+                    mediaPlayer.seekTo(0);/*
+                    mediaPlayer.release();*/
+                    if (mediaPlayer.isPlaying()) {
+                        sbProgress.postDelayed(seekBarUpdater, 100);
+                        ivPlayPause.setImageResource(R.drawable.pause_icon);
+                        ltPlayPause.setAnimation(R.raw.play_to_pause);
+                    } else {
+                        sbProgress.removeCallbacks(seekBarUpdater);
+                        ivPlayPause.setImageResource(R.drawable.play_icon);
+                        ltPlayPause.setAnimation(R.raw.pause_to_play);
+                    }
+                }
+            });
+            app.l("exit");
+            progressCirculaSound.setVisibility(View.GONE);
+            percentDone.setVisibility(View.GONE);
+            startDownload.setVisibility(View.GONE);
+            ivPlayPause.setVisibility(View.VISIBLE);
+            ltPlayPause.setVisibility(View.VISIBLE);
+            ltPlayPause.setAnimation(R.raw.pause_to_play);
+            ltPlayPause.playAnimation();
+            sbProgress.setEnabled(false);
+
+        }
+
+        File tmpFile = new File(context.getExternalFilesDir("voice/mp3").getAbsolutePath() + url.substring(url.lastIndexOf("/")) + ".temp");
+        if (tmpFile.exists()) {
+            sbProgress.setEnabled(false);
+            progressCirculaSound.setVisibility(View.VISIBLE);
+            percentDone.setVisibility(View.VISIBLE);
+            startDownload.setAlpha(0f);
+            ivPlayPause.setVisibility(View.GONE);
+            ltPlayPause.setVisibility(View.GONE);
+            int progress = (int) (tmpFile.length() * 100 / goood.getFileSize());
+            app.l(progress + "____");
+            progressCirculaSound.stopRotation();
+            progressCirculaSound.setProgress(progress);
+            progressCirculaSound.setSpeed(0f);
+            String text = progress + "%";
+            percentDone.setText(text);
+        }
+
+        /*if (mediaPlayer != null) {
+            ltPlayPause.setAnimation(mediaPlayer.isPlaying() ? R.raw.play_to_pause : R.raw.pause_to_play);
+            ltPlayPause.pauseAnimation();
+        }*/
+
+        startDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playManager(goood);
+            }
+        });/**/
 
 
+        ivPlayPause.setOnClickListener(this);
+        ltPlayPause.setOnClickListener(this);
+        sbProgress.setOnSeekBarChangeListener(this); /* */
+        initComponent();
+    }
+
+    private void initViewPager() {
         String thumbAddressesInString = goood.getThumbnails();
         String[] splitThumb = thumbAddressesInString.split(",");
         String[] splitAll = new String[splitThumb.length + 1];
@@ -88,9 +213,99 @@ public class ShowGoodDetailActivity extends AppCompatActivity {
         viewpager.setOffscreenPageLimit(3);
 
         extensiblePageIndicator.initViewPager(viewpager);
+    }
+
+    private void initViews() {
+        extensiblePageIndicator = findViewById(R.id.flexibleIndicator);
+        viewpager = findViewById(R.id.viewpager);
+        goodName = findViewById(R.id.good_name);
+        carIcon = findViewById(R.id.car_icon);
+        suitableCars = findViewById(R.id.suitable_cars);
+        factoryIcon = findViewById(R.id.factory_icon);
+        companyName = findViewById(R.id.company_name);
+        countryName = findViewById(R.id.country_name);
+        warrantyIcon = findViewById(R.id.warranty_icon);
+        warrantyName = findViewById(R.id.warranty_name);
+        stateIcon = findViewById(R.id.state_icon);
+        stateText = findViewById(R.id.state_text);
+        parent = findViewById(R.id.parent);
+        desc = findViewById(R.id.desc);
+
+
+        startDownload = findViewById(R.id.startDownload);
+        progressCirculaSound = findViewById(R.id.progressCirculaSound);
+        percentDone = findViewById(R.id.percentDone);
+        ivPlayPause = findViewById(R.id.ivPlayPause);
+        sbProgress = findViewById(R.id.sbProgress);
+        ltPlayPause = findViewById(R.id.ltPlayPause);
+        price = findViewById(R.id.price);
 
     }
 
+    private void playManager(Goood good) {
+        ltPlayPause.pauseAnimation();
+        startDownload.setAlpha(0f);
+        String url = good.getVoice();
+        String path = getExternalFilesDir("voice/mp3").getAbsolutePath();
+
+        int downloadId = SharedPrefUtils.getIntData("soundDownloadId**" + good.getId());
+        if (Status.RUNNING == PRDownloader.getStatus(downloadId)) {
+            PRDownloader.pause(downloadId);
+            app.l("AAAAA");
+            progressCirculaSound.setSpeed(0.000001f);
+            return;
+        }
+        if (Status.PAUSED == PRDownloader.getStatus(downloadId)) {
+            PRDownloader.resume(downloadId);
+            progressCirculaSound.startRotation();
+            progressCirculaSound.setSpeed(4f);
+            return;
+        }
+
+        DownloadRequest downloadRequest = PRDownloader.download(url, path, url.substring(url.lastIndexOf("/"))).build();
+        downloadRequest.setOnPauseListener(() -> {
+        }).setOnProgressListener(progress -> {
+            int value = (int) (100 * progress.currentBytes / progress.totalBytes);
+            progressCirculaSound.setProgress(value);
+            String text = value + "%";
+            percentDone.setText(text);
+        }).setOnStartOrResumeListener(() -> {
+            progressCirculaSound.setVisibility(View.VISIBLE);
+            progressCirculaSound.setSpeed(4f);
+        });
+        downloadId = downloadRequest.start(new OnDownloadListener() {
+            @Override
+            public void onDownloadComplete() {
+                SharedPrefUtils.getSharedPrefEditor(SharedPrefUtils.PREF_APP).remove("soundDownloadId**" + good.getId()).apply();
+                progressCirculaSound.setSpeed(0f);
+                progressCirculaSound.setVisibility(View.GONE);
+                startDownload.setVisibility(View.GONE);
+                percentDone.setVisibility(View.GONE);
+                ivPlayPause.setVisibility(View.VISIBLE);
+                ltPlayPause.setVisibility(View.VISIBLE);
+                sbProgress.setEnabled(true);
+
+                app.l("completed");
+            }
+
+            @Override
+            public void onError(Error error) {
+
+            }
+        });
+        SharedPrefUtils.saveData("soundDownloadId**" + good.getId(), downloadId);
+    }
+
+    /**/
+    private class SeekBarUpdater implements Runnable {
+        @Override
+        public void run() {
+            if (mediaPlayer != null) {
+                sbProgress.setProgress(mediaPlayer.getCurrentPosition());
+                sbProgress.postDelayed(this, 100);
+            }
+        }
+    }
 
     private void renderData(String[] body) {
         for (String url : body) {
@@ -101,25 +316,25 @@ public class ShowGoodDetailActivity extends AppCompatActivity {
         //pageSwitcher(body.length);
     }
 
-
-    public void pageSwitcher(int size) {
-        new Timer().scheduleAtFixedRate(new TimerTask() {
+    private void initComponent() {
+        final LinearLayout image = findViewById(R.id.ll);
+        final CollapsingToolbarLayout collapsing_toolbar = findViewById(R.id.collapsing_toolbar);
+        ((AppBarLayout) findViewById(R.id.app_bar_layout)).addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
             @Override
-            public void run() {
-                ci++;
-                if (ci == size)
-                    ci = 0;
-                runOnUiThread(() -> {
-                    app.l(ci);
-                    viewpager.setCurrentItem(ci);
-                });
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                int min_height = ViewCompat.getMinimumHeight(collapsing_toolbar) * 2;
+                float scale = (float) (min_height + verticalOffset) / min_height;
+//                app.l(verticalOffset+"SSS"+min_height);
+//                image.setScaleX(scale >= 0 ? scale : 0);
+                image.setScaleY(scale >= 0 ? scale : 0);
+
             }
-        }, 1000, 5000);
+        });
     }
 
-
-
     void binder(Goood goood) {
+        desc.setText(goood.getGood_desc());
+        price.setText(goood.getPrice() + "");
         goodName.setText(goood.getGood_id());
         Gson gson = new Gson();
         Car[] cars = gson.fromJson(goood.getSuitable_car(), Car[].class);
@@ -128,9 +343,14 @@ public class ShowGoodDetailActivity extends AppCompatActivity {
         countryName.setText(goood.getMade_by());
         warrantyName.setText(goood.getWarranty());
         if (goood.getIs_stock() == 0 && goood.getStatus() == 0) {
+            stateText.setText("این کالا در حال حاضر موجود نمی باشد");
+            stateText.setVisibility(View.VISIBLE);
             stateIcon.setVisibility(View.VISIBLE);
         } else if (goood.getIs_stock() == 2 && goood.getStatus() == 1) {
 
+            stateText.setVisibility(View.VISIBLE);
+            stateText.setText(activity.getResources().getString(R.string.luxury_good));
+            stateText.setTextColor(activity.getResources().getColor(R.color.yellow_900));
 
             stateIcon.setVisibility(View.VISIBLE);
             stateIcon.setImageDrawable(activity.getDrawable(R.drawable.ic_diamond));
@@ -138,12 +358,16 @@ public class ShowGoodDetailActivity extends AppCompatActivity {
 
 
         } else if (goood.getIs_stock() == 1 && goood.getStatus() == 1) {
+            stateText.setVisibility(View.VISIBLE);
+            stateText.setText(activity.getResources().getString(R.string.stoke_good));
+            stateText.setTextColor(activity.getResources().getColor(R.color.red_full));
 
             stateIcon.setVisibility(View.VISIBLE);
             stateIcon.setImageDrawable(activity.getDrawable(R.drawable.ic_nis));
             stateIcon.setColorFilter(activity.getResources().getColor(R.color.red_full));
 
         } else if (goood.getIs_stock() == 0 && goood.getStatus() == 1) {
+            stateText.setVisibility(View.INVISIBLE);
             stateIcon.setVisibility(View.INVISIBLE);
         }
 
@@ -166,5 +390,91 @@ public class ShowGoodDetailActivity extends AppCompatActivity {
 
         suitableCars.setText(carsText.toString());
     }
+
+    /**/
+    private void updatePlayingView() {
+        sbProgress.setMax(mediaPlayer.getDuration());
+        sbProgress.setProgress(mediaPlayer.getCurrentPosition());
+        sbProgress.setEnabled(true);
+        if (mediaPlayer.isPlaying()) {
+            sbProgress.postDelayed(mUpdateSeekbar, 100);
+            ivPlayPause.setImageResource(R.drawable.pause_icon);
+            ltPlayPause.setAnimation(R.raw.play_to_pause);
+        } else {
+            sbProgress.removeCallbacks(mUpdateSeekbar);
+            ivPlayPause.setImageResource(R.drawable.play_icon);
+            ltPlayPause.setAnimation(R.raw.pause_to_play);
+        }
+        ltPlayPause.playAnimation();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ivPlayPause:
+            case R.id.ltPlayPause: {
+
+                if (mediaPlayer == null) {
+                    app.l("AA");
+                    startMediaPlayer(audioAddress);
+                } else {
+                    app.l("BB");
+
+                    if (mediaPlayer.isPlaying()) {
+                        app.l("CC");
+                        mediaPlayer.pause();
+                    } else {
+                        app.l("DD");
+                        mediaPlayer.start();
+                    }
+                }
+
+                /**/
+                updatePlayingView();
+            }
+            break;
+        }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if (fromUser) {
+            mediaPlayer.seekTo(progress);
+        }
+    }
+
+    private void startMediaPlayer(String audioAddress) {
+        mediaPlayer = MediaPlayer.create(Application.getContext(), Uri.parse(audioAddress));
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mediaPlayer.release();
+            }
+        });
+        mediaPlayer.start();
+    }
+
+    private void releaseMediaPlayer() {
+        mediaPlayer = null;
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            releaseMediaPlayer();
+        }
+        super.onBackPressed();
+    }/* */
 }
 
--- app/src/main/java/com/example/mechanic2/activities/TestActivity.java
+++ app/src/main/java/com/example/mechanic2/activities/TestActivity.java
@@ -2,6 +2,7 @@ package com.example.mechanic2.activities;
 
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.icu.util.Measure;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,22 +28,16 @@ import com.nightonke.wowoviewpager.WoWoViewPager;
 import com.nightonke.wowoviewpager.WoWoViewPagerAdapter;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
+import me.itangqi.waveloadingview.WaveLoadingView;
 
 public class TestActivity extends Activity {
 
 
 
 
-    NumberPicker np1, np2;
-    TextView tv1;
 
-    String selectedValue = "Select Value";
-    String selectedBrand = "Select Brand";
 
-    int priceApple = 3000;
-    int priceLG = 1000;
-    int priceSONY = 1500;
-
+    private SeekBar seekbar;
 
 
     @Override
@@ -49,63 +45,6 @@ public class TestActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        tv1 = findViewById(R.id.tv1);
-        np1 = findViewById(R.id.np1);
-        np2 = findViewById(R.id.np2);
-
-        final String[] mBrand = {"Select Brand", "Apple", "LG", "SONY"};
-        np1.setMinValue(0);
-        np1.setMaxValue(mBrand.length - 1);
-        np1.setWrapSelectorWheel(true);
-        np1.setDisplayedValues(mBrand);
-        np1.setValue(0);
-
-        np1.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                selectedBrand = mBrand[newVal];
-                calculateCost(selectedBrand, selectedValue);
-
-            }
-        });
-
-        final String[] mValue = {"Select Value", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
-        np2.setMinValue(0);
-        np2.setMaxValue(mValue.length - 1);
-        np2.setWrapSelectorWheel(true);
-        np2.setDisplayedValues(mValue);
-        np2.setValue(0);
-
-        np2.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                selectedValue = mValue[newVal];
-                calculateCost(selectedBrand, selectedValue);
-            }
-        });
     }
 
-    void calculateCost(String selectedBrand, String selectedValue) {
-        if (!selectedValue.equals("Select Value")) {
-            switch (selectedBrand) {
-                case "Apple":
-                    app.l(selectedValue + priceApple);
-                    tv1.setText("price= " + priceApple * Integer.parseInt(selectedValue));
-                    break;
-                case "LG":
-                    app.l(selectedValue + priceLG);
-                    tv1.setText("price= " + priceLG * Integer.parseInt(selectedValue));
-                    break;
-                case "SONY":
-                    tv1.setText("price= " + priceSONY * Integer.parseInt(selectedValue));
-                    app.l(selectedValue + priceSONY);
-                    break;
-                default:
-                    tv1.setText("Error");
-                    break;
-            }
-        } else tv1.setText("Error");
-
-
-    }
 }
